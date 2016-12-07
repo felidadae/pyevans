9f7316311d7a12e5cd8467846419b2dd6068c2ec
Szymon Bugaj <felidadae@gmail.com>
2016-12-07 12:12:18 +0100

    Added loader of midi files; simple heuristic to extract melody;

--- pyevans/miditools/loader.py
+++ pyevans/miditools/loader.py
@@ -1,5 +1,6 @@
 from mido import MidiFile, MetaMessage
 import time
+import operator
 
 from miditools.connector import *
 setMidoBackend()
@@ -33,15 +34,43 @@ def convertMidiFileToMonoMelody(filePath):
 	tracks = midfref.tracks
 	indexOfChoosenTrack = findTrackWithMaxEvents(tracks) 
 
-	for message in tracks[indexOfChoosenTrack]:
+	previousMessage = 0 
+	accord = []
+
+	chTrack = tracks[indexOfChoosenTrack]
+	for im, message in enumerate(chTrack):
 		if not isinstance(message, MetaMessage):
-			if (message.type != 'note_on' and 
-					message.type != 'note_off'):
+			if (message.type != 'note_on' and message.type != 'note_off'):
 				continue
 
-			# if (message.time == 0 ):
-			#  	continue
+			if (message.time == 0):
+				accord.append(message)
+				continue
+			if (len(accord) > 0 and message.time > 0 and not(type(previousMessage) is int)):
+				print (message)
+				time.sleep(message.time/400.0)
 
+				diff = [abs(previousMessage.note-m.note) for m in accord]
+				index, value = min(enumerate(diff), key=operator.itemgetter(1))
+				chn = accord[index]
+				port.send(accord[index])
+				cmessage = mido.Message('note_off', 
+					note=chn.note, velocity=chn.velocity)
+				port.send(cmessage)
+				previousMessage = chn
+				accord = []
+				continue
+				
+			if (not(type(previousMessage) is int) and 
+					abs(message.note - previousMessage.note) > 8 ):
+				time.sleep(message.time/400.0)
+				continue
+
+
+			previousMessage = message
 			print (message)
 			time.sleep(message.time/400.0)
 			port.send(message)
+			cmessage = mido.Message('note_off', 
+				note=message.note, velocity=message.velocity)
+			port.send(cmessage)
